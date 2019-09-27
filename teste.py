692767b847875d194235f5dc62437331d8037b3b
preichmann <preichmann@bionexo.com>
2019-09-27 16:46:13 -0300

    Alterações para se adequar as questões de permissionamento da AWS

--- teste.py
+++ teste.py
@@ -3,30 +3,34 @@ import boto3
 import json
 import SimpleHTTPServer
 import SocketServer
- 
+
+
 PORT = 8201
 
-client = boto3.client('athena')
+client = boto3.client('athena', region_name='us-east-1')
 
 queryId = client.start_query_execution(
-	QueryString='SELECT SUM(line_item_blended_cost) FROM awsbilling WHERE line_item_resource_id like \'%snap-%\' AND cast(month(current_date)AS DECIMAL) = cast(month AS DECIMAL) group by month order by month desc;',
-	QueryExecutionContext={
-		'Database': 'default'
-	},
-	WorkGroup='ANALYTICS_USERS'
+        QueryString='SELECT SUM(line_item_blended_cost) FROM default.awsbilling WHERE line_item_resource_id like \'%snap-%\' AND cast(month(current_date)AS DECIMAL) = cast(month AS DECIMAL) group by month order by month desc',
+        ResultConfiguration={
+                'OutputLocation': 's3://aws-athena-query-results-701085748382-us-east-1/users/',
+                'EncryptionConfiguration': {
+            'EncryptionOption': 'SSE_S3'
+            }
+        },
+        WorkGroup='ANALYTICS_USERS'
 )["QueryExecutionId"]
 
 while(client.get_query_execution(QueryExecutionId=queryId) == 'RUNNING'):
-	time.sleep(1)
-	print "durmiu 1s"
+        time.sleep(1)
+        print "durmiu 1s"
 else:
-	status = client.get_query_execution(QueryExecutionId=queryId)["QueryExecution"]["Status"]["State"]
-	if(status != 'SUCCEEDED'):
-		queryResult = status
-	else:
-		queryResult = client.get_query_results(
-			QueryExecutionId=queryId
-		)["ResultSet"]["Rows"][1]["Data"][0]["VarCharValue"]
+        status = client.get_query_execution(QueryExecutionId=queryId)["QueryExecution"]["Status"]["State"]
+        if(status != 'SUCCEEDED'):
+                queryResult = status
+        else:
+                queryResult = client.get_query_results(
+                        QueryExecutionId=queryId
+                )["ResultSet"]["Rows"][1]["Data"][0]["VarCharValue"]
 
 
 class my_handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
@@ -36,14 +40,14 @@ class my_handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
       self.end_headers()
       self.wfile.write("<a>"+queryResult+"</a>")
       return
- 
- 
+
+
 try:
    httpd = SocketServer.ThreadingTCPServer(('', PORT), my_handler)
- 
+
    print "servidor web rodando na porta ", PORT
    httpd.serve_forever()
- 
+
 except KeyboardInterrupt:
    print "Voce pressionou ^C, encerrando..."
    httpd.socket.close()
